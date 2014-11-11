cd5aa10648ce49282ef9f20ed83c0e7598dd1847
Mihir Shete <mihirsht@gmail.com>
2014-11-11 11:09:58 +0000

    dotfiles: add Vundle.vim as a submodule
    vundle was being cloned by init script.
    CHange that, make sure that Vundle.vim is a
    submodule.

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,6 +1,6 @@
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
-  if [ ! -f $HOME/.vim/bundle/vundle/README.md ]; then
+  if [ ! -f $HOME/.vim/bundle/Vundle.vim/README.md ]; then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   fi
     vim +PluginInstall +qall
